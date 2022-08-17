0fe81e67e674b975b0d97b9219f4692356b047d8
MariaCz <maria.czapczyk@gmail.com>
2022-08-17 19:32:49 +0200

    updates

--- data/data_modification.py
+++ data/data_modification.py
@@ -1,6 +1,5 @@
 import sys
 import os.path
-from typing import NoReturn
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
@@ -9,9 +8,7 @@ import pandas as pd
 
 def short_raw_data(nrows):
     df = pd.read_csv(app.DATA_URL, nrows=nrows)
-    #excelWriter = pd.ExcelWriter(f'{app.DATA_URL[:-3]}_min.csv')
     df.to_csv('Motor_Vehicle_Collisions_-_Crashes_min.csv', index=False)
-    #excelWriter.save()
-    #excelWriter.close()
+
     
 short_raw_data(90000)
